c651d110e30086247f513dce003aeabb785feea7
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-11 15:49:55 -0400

    loadtweets function will use jQuery to make a request to /tweets and receive the array of tweets as JSON

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 $( document ).ready(function() {
 
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const data = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png"
-            ,
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": "https://i.imgur.com/nlhLi3I.png",
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        }
-      ]
-
 const renderTweets = function(tweets) {
     // loops through tweets
     for (let tweet of tweets) {
@@ -47,13 +21,13 @@ const createTweetElement = function(tweet) {
     let $tweet = $(`<article class="tweet">
     <header>
       <div class="tweet-user">
-      <img src="https://i.imgur.com/z5LNkkB.png">
-      <p>Newton</p></div>
-      <p>@Sirlsaac</p>
+      <img src=${tweet.user.avatars}>
+      <p>${tweet.user.name}</p></div>
+      <p>${tweet.user.handle}</p>
     </header>
-   
+    <p>${tweet.content.text}</p>
     <footer>
-      <p>10 days ago</p>
+      <p> ${timeago.format(tweet.created_at)} </p>
       <div class="footer-icons">
       <i class="fas fa-heart"></i>
       <i class="fas fa-flag"></i>
@@ -63,7 +37,9 @@ const createTweetElement = function(tweet) {
   </article>`)
   return $tweet;
 }
-renderTweets(data);
+// renderTweets(data);
+
+// timeago.format(1473245023718);   
 
 $("#tweetForm").submit(function(event) {
     event.preventDefault();
