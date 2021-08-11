f6ec90210612ae0b4c42fb87d99426f457bc1190
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-11 17:02:27 -0400

    add the escape function in client.js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,17 @@
+
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $( document ).ready(function() {
+
+const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
 
 const renderTweets = function(tweets) {
     // loops through tweets
@@ -18,6 +26,7 @@ const renderTweets = function(tweets) {
 
 
 const createTweetElement = function(tweet) {
+    let text = tweet.content.text;
     let $tweet = $(`<article class="tweet">
     <header>
       <div class="tweet-user">
@@ -25,7 +34,8 @@ const createTweetElement = function(tweet) {
       <p>${tweet.user.name}</p></div>
       <p>${tweet.user.handle}</p>
     </header>
-    <p>${tweet.content.text}</p>
+
+    <p>${escape(text)}</p>
     <footer>
       <p> ${timeago.format(tweet.created_at)} </p>
       <div class="footer-icons">
@@ -45,8 +55,30 @@ $("#tweetForm").submit(function(event) {
     event.preventDefault();
     let result = $(this).serialize()
     $.post ("/tweets", result);
+    let tweetMessage = $("#tweet-text").val()
+    let datalength = tweetMessage.length;
+    if (datalength > 140) {
+        // alert("please keep your message under 140 charcters");
+        $(".alert").text("Please keep your message under 140 charcters").slideDown(() => {
+            setTimeout(() => {
+                $('.alert').slideUp();
+            }, 3000);
+        })
+    } else if (datalength === 0) {
+        // alert("Tweet is empty")
+        $(".alert").text("Tweet is empty").slideDown(() => {
+            setTimeout(() => {
+                $('.alert').slideUp();
+            }, 3000);
+        })
+    } else {
+        $.ajax ({
+        url: "/tweets", // sending the data
+        method: "POST", // post request
+        data: $(this).serialize() // serialized the data
+        })
+    }
 })
-
 // Renders the tweets if the GET request was successfull
 
 const loadTweets = function() {
